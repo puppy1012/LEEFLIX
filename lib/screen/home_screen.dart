5a90103d2e3b10928b656f67591704bf12a547b8
Seunghyun Lee <puppy1012@naver.com>
2025-01-05 00:15:01 +0900

    상단바 위젯 작업

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initSate() {
+  void initState() {
     super.initState();
   }
 
@@ -17,18 +17,47 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+// 상단 바(TopBar) 위젯
 class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Container: 상하좌우 여백 설정
     return Container(
       padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      // Row: 가로 방향 배치, spaceBetween으로 양 끝에 정렬
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
+          // 로고 이미지
           Image.asset(
             'images/bbongflix_logo.png',
             fit: BoxFit.contain,
             height: 25,
-          )
+          ),
+          // 메뉴 탭: TV 프로그램
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          // 메뉴 탭: 영화
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          // 메뉴 탭: 내가찜한 목록
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가찜한 목록',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
         ],
       ),
     );
