590303b6fdf7039d4aced2785830bd391aa57c30
Seunghyun Lee <puppy1012@naver.com>
2025-01-07 19:17:22 +0900

    firebased 연동작업중

--- lib/model/model_movie.dart
+++ lib/model/model_movie.dart
@@ -1,15 +1,21 @@
+// import '';
+
 class Movie {
   final String title;
   final String keyword;
   final String poster;
   final bool like;
-
+  // final DocumentReference reference;
   Movie.formMap(Map<String, dynamic> map)
       : title = map['title'],
         keyword = map['keyword'],
         poster = map['poster'],
         like = map['like'];
 
+  // Movie.fromSnapshot(DocumentSnapshot snapshot):
+  //     this.formMap(snapshot.data, reference: snapshot.reference);
   @override
   String toString() => "Movie<$title:$keyword";
 }
+
+
